11d44f1606de3562e9a5b822fe4858966db89e06
Marshall Offutt <marshalloffutt@gmail.com>
2019-05-20 15:30:46 -0500

    try-except block for reading and printing lists

---
+++ cats_and_dogs.py
@@ -0,0 +1,15 @@
+def read_pets(filename):
+    """Print the names contained in the supplied filename"""
+    try:
+        with open(filename) as file_object:
+            contents = file_object.read()
+            print(contents)
+    except FileNotFoundError:
+        message = "Sorry, the file " + filename + " was not found."
+        print(message)
+
+# Store the lists into a variable
+filenames = ['text_files/cats.txt', 'text_files/dogs.txt']
+
+for filename in filenames:
+    read_pets(filename)
