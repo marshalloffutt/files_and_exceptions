99bfd3dc2ba31821317248935e25039791c19d0d
Marshall Offutt <marshalloffutt@gmail.com>
2019-05-20 16:09:35 -0500

    refactor recall_number

--- favorite_number.py
+++
@@ -1,8 +0,0 @@
-import json
-
-number = input("What is your favorite number? \n")
-
-filename = 'number.json'
-with open(filename, 'w') as f_obj:
-    json.dump(number, f_obj)
-    print("Thank you. I shall never forget this.")
