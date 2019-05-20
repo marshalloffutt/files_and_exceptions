91dd88a1777cecfc4db8843ae8e5758d61d564c9
Marshall Offutt <marshalloffutt@gmail.com>
2019-05-20 10:59:02 -0500

    exercise 10-1

---
+++ learning_python.py
@@ -0,0 +1,23 @@
+# Store file into a variable
+filename = 'learning_python.txt'
+
+# Read entire file
+with open(filename) as file_object:
+    contents = file_object.read()
+    print(contents.rstrip())
+
+print("\n")
+
+# Read line by line
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+print("\n")
+
+# Make a list of lines from file
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.strip())
